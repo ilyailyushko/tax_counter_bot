80caa1b8e811f0faac710a496ae0225245350b81
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2020-05-31 02:23:24 +0300

    Подправил чтоб вывод постов бота был более читабельным для юзера

--- bot.py
+++ bot.py
@@ -3,12 +3,12 @@ import telebot
 bot = telebot.TeleBot("1217064730:AAECUm0tyh9z9SEz3i1y3BNKN7aN2dd_TuM")
 original_sum = ''
 original_tax = ''
+welcome_msg = "Если надо выстваить счет юр лицу 👉 /6" + '\n\n' + "А если физ лицу 👉 /4"
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.send_message(message.chat.id,
-        "Если вы собираетесь выстваить счет юр лицу 👉 /6 если физ лицу 👉 /4")
+    bot.send_message(message.chat.id, welcome_msg)
 
 
 @bot.message_handler(commands=['4', '6'])
@@ -16,10 +16,10 @@ def fix_tax(message):
     global original_tax
     if message.text=='/4':
         original_tax = 4
-        bot.reply_to(message, "Ок, физ лицо, будем платить 4% ")
+        bot.reply_to(message, "👌 Физ лицо ➡️ будем платить 4% ")
     if message.text=='/6':
         original_tax = 6
-        bot.reply_to(message, "Ок, юр лицо, заплатим 6% налога ")
+        bot.reply_to(message, "👌 Юр лицо ➡️ заплатим 6% налога ")
     msg = bot.send_message(message.chat.id, "Какая сумма должна зайти чистыми?")
     bot.register_next_step_handler(msg, fix_sum)
 
@@ -34,12 +34,16 @@ def fix_sum(message):
         if total_sum_after_tax < original_sum:
             total_sum = total_sum + 1
             total_sum_after_tax = (total_sum - (total_sum / 100 * original_tax))
-        msg = 'Надо выставить счет на 👉' + str(total_sum) + "₽"
-        msg = msg + 'Сумма налога 👉' + str("%.2f" % (total_sum - total_sum_after_tax)) + "₽"
+        ok = '✅'
+        msg = 'Надо выставить счет на 👉' + str(total_sum) + '\n\n'
+        msg = msg + 'Сумма налога 👉' + str("%.2f" % (total_sum - total_sum_after_tax)) + "₽" + '\n\n'
         msg = msg + 'После уплаты налогов останется 👉' + str(total_sum_after_tax) + "₽"
-        bot.send_message(message.chat.id, msg, total_sum)
+
+        bot.send_message(message.chat.id, ok)
+        bot.send_message(message.chat.id, msg)
     except Exception as e:
-        bot.reply_to(message, 'Это не число или что то пошло не так...')
+        bot.reply_to(message, '🤷‍♂️🔡 Это не число или что то пошло не так...')
+        bot.reply_to(message, 'Давай начнием сначала' + '\n\n' + welcome_msg)
 
 
 bot.polling()
